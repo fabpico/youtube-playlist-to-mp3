10f0fa421db47a12f1103072a3ec723d9b4d1a63
Fabian Picone <fabian.picone@gmail.com>
2022-04-09 17:13:09 +0200

    [#3] Convert all mp4 to mp3

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local
 RUN apt-get update
 RUN apt-get install -y git
 RUN pecl install xdebug-3.1.4 && docker-php-ext-enable xdebug
+RUN apt-get install -y ffmpeg
--- cli.php
+++ cli.php
@@ -6,10 +6,12 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\HttpClient\CurlHttpClient as SymfonyCurlHttpClient;
 use Fabpico\YoutubePlaylistToMp3\Common\Httpclient\CurlHttpClient;
 use Fabpico\YoutubePlaylistToMp3\Commands\DownloadVideosCommand;
+use Fabpico\YoutubePlaylistToMp3\Commands\ConvertMp4ToMp3Command;
 
 $application = new Application();
 
 // todo, autowire
 $httpClient = new CurlHttpClient(new SymfonyCurlHttpClient());
 $application->add(new DownloadVideosCommand($httpClient));
+$application->add(new ConvertMp4ToMp3Command());
 $application->run();
